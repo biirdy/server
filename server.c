b0be80134c64a7fbb0ab82eede0b25da8faad1e7
Jamie Bird <jamie06bird@gmail.com>
2014-11-03 16:33:19 +0000

    Simple client and server.

---
+++ server.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include <sys/time.h>
+
+int main (int argc, char ** argv) {
+
+	int welcomeSocket, newSocket;
+	char buffer[1024];
+	struct sockaddr_in serverAddr, clientAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size;
+
+	/*---- Create the socket. The three arguments are: ----*/
+	/* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+	welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+	/*---- Configure settings of the server address struct ----*/
+	/* Address family = Internet */
+	serverAddr.sin_family = AF_INET;
+	/* Set port number, using htons function to use proper byte order */
+	serverAddr.sin_port = htons(7891);
+	/* Set IP address to localhost */
+	serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	/* Set all bits of the padding field to 0 */
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+	/*---- Bind the address struct to the socket ----*/
+	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+	/*---- Listen on the socket, with 5 max connection requests queued ----*/
+	if(listen(welcomeSocket,5)==0)
+		printf("Listening\n");
+	else
+		printf("Error\n");
+
+	/*---- Accept call creates a new socket for the incoming connection ----*/
+	addr_size = sizeof serverStorage;
+
+	while(1){
+		newSocket = accept(welcomeSocket, (struct sockaddr *) &clientAddr, &addr_size);
+		if(fork() == 0){
+			char addr[15];
+			printf("Connected %s with pid %d\n", inet_ntop(AF_INET, &clientAddr.sin_addr, addr, addr_size), (int) getpid());
+
+			struct timeval tv;
+
+			int bytes = 1;
+			
+			while(bytes){
+				fd_set rfds;
+				FD_ZERO(&rfds);
+				FD_SET (newSocket, &rfds);
+
+				tv.tv_sec = 5;
+				tv.tv_usec = 0;
+				
+				int ready = select(newSocket + 1, &rfds, NULL, NULL, &tv);
+				
+				if(ready == -1){
+					perror("select()\n");
+				}else if(ready){
+					bytes = recv(newSocket,buffer,13,0);
+					if(strcmp(buffer, "Heartbeat") == 0){
+						printf("Heartbeat from %s %d - %s\n", inet_ntop(AF_INET, &clientAddr.sin_addr, addr, addr_size), (int) getpid(), buffer);
+					}else{
+						printf("XXX\n");
+					}
+					//printf("%d\n", strcmp("Heartbeat", buffer));	
+				}else{
+					printf("Timeout\n");
+					break;
+				}
+					
+			}
+			printf("Connection %s %d closed\n", inet_ntop(AF_INET, &clientAddr.sin_addr, addr, addr_size), (int) getpid());
+			close(newSocket);
+		}
+	}
+
+	close(welcomeSocket);
+
+	return 0;
+}
