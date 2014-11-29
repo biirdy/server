85944cdebce81d24653dbebb60a37b1bd1c49f53
Jamie Bird <jamie06bird@gmail.com>
2014-11-29 23:57:21 +0000

    ping

--- server.c
+++ server.c
@@ -116,6 +116,7 @@ int main(int argc, char ** argv) {
 
 			//call initial ping 
 			char * command = "../tools/ping %d %d %s";
+			char cmd[50];
 			sprintf(cmd, command, id, addr);
 			system(cmd);
 
